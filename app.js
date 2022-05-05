faaab91e171afc344cac4f1783d747c2124d5aed
FernandoMig <fcmiguez@yahoo.com>
2022-05-05 12:56:25 -0300

    subida de carrito

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, 'public')))
 
-app.listen(5000, () => {
-    console.log('Servidor corriendo en el puerto 5000');
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
 });
 
 app.get('/', (req, res) => {
