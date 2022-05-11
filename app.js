9fbb1fa120d0460aabdf161ff5ccd86a61c04cd3
antariex <arielantequiera@gmail.com>
2022-05-11 19:30:07 -0300

    app.js reparado

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, 'public')))
 
-app.listen(5000, () => {
-    console.log('Servidor corriendo en el puerto 5000');
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
 });
 
 app.get('/', (req, res) => {
@@ -26,4 +26,4 @@ app.get('/views/productDetail.html', (req, res) => {
 
 app.get('/views/register.html', (req, res) => {
     res.sendFile(path.join(__dirname, "./views/register.html"))
-})
+}) 
