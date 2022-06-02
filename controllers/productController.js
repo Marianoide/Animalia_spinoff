aebf22cbf8b6266132e68d66bd67f4fffe0d5305
antariex <arielantequiera@gmail.com>
2022-06-02 18:05:56 -0300

    Cambios en rutas y controladores

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,20 +10,81 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const productController = {
 
     catalogo: (req, res, next) => {
-        res.render('./products/products', {products} );
+        res.render('./products/products', {
+            products
+        });
     },
+
     detalle: (req, res, next) => {
-        let productoBuscado = products.find( producto => {
-         return producto.id == req.params.id
+        let productoBuscado = products.find(producto => {
+            return producto.id == req.params.id
         })
-        res.render("./products/productDetail",{producto : productoBuscado});
-     },
+        res.render("./products/productDetail", {
+            producto: productoBuscado
+        });
+    },
+
     creacion: (req, res, next) => {
         res.render('./products/productCreate');
     },
+
+    almacenar: (req, res, next) => {
+        let image
+        if (req.files[0] != undefined) {
+            image = req.files[0].filename
+        } else {
+            image = "/images/default-image.png"
+        }
+        let nuevoProducto = {
+            id: products[products.length - 1].id + 1,
+            ...req.body,
+            image: image
+        }
+        products.push(nuevoProducto)
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ''));
+        res.redirect("/products")
+    },
+
     edicion: (req, res, next) => {
+        let id = req.params.id
+        let product = products.find(product => product.id == id)
         res.render('./products/productEdit');
     },
+
+    actualizar: (req, res) => {
+        let id = req.params.id
+        let productToEdit = products.find(product => product.id == id)
+        console.log("🚀 ~ archivo: productsController.js ~ req.files", req.files)
+        let image
+        if (req.files[0] != undefined) {
+            image = req.files[0].filename
+        } else {
+            image = productToEdit.image
+        }
+        productToEdit = {
+            id: productToEdit.id,
+            ...req.body,
+            image: image,
+        }
+        let newProduct = products.map(product => {
+            if (product.id == productToEdit.id) {
+                return product = {
+                    ...productToEdit
+                };
+            }
+            return product
+        })
+        fs.writeFileSync(productsFilePath, JSON.stringify(newProduct));
+        res.redirect("/views/products/productDetail.ejs" + productToEdit.id)
+    },
+
+    borrado: (req, res, next) => {
+        let id = req.params.id
+        let productoABorrar = products.filter(product => product.id != id)
+        fs.writeFileSync(productsFilePath, JSON.stringify(productoABorrar));
+        res.redirect("/")
+    }
+
 };
 
 module.exports = productController;
